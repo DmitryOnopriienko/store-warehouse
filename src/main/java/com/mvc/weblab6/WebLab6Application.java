54f2608c7d8bc1cd6917d9baf504a850132f7bc8
Dmitry Onopriienko <dmitry.onopriienko@gmail.com>
2023-06-05 12:22:11 +0300

    Init

---
+++ src/main/java/com/mvc/weblab6/WebLab6Application.java
@@ -0,0 +1,13 @@
+package com.mvc.weblab6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebLab6Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WebLab6Application.class, args);
+  }
+
+}
---
+++ src/test/java/com/mvc/weblab6/WebLab6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvc.weblab6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebLab6ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
