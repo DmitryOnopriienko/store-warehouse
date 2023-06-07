48a1cd825d8f86f31f28fc5b14ce9066646e1d79
Dmitry Onopriienko <dmitry.onopriienko@gmail.com>
2023-06-07 20:16:27 +0300

    Change Product REST controller to MVC, add view templates

--- src/main/java/com/mvc/weblab6/controller/ProductController.java
+++ src/main/java/com/mvc/weblab6/controller/ProductController.java
@@ -3,14 +3,13 @@ package com.mvc.weblab6.controller;
 import com.mvc.weblab6.entity.Product;
 import com.mvc.weblab6.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
-@RestController
+@Controller
 @RequestMapping("/products")
 public class ProductController {
 
@@ -22,12 +21,14 @@ public class ProductController {
   }
 
   @GetMapping
-  public List<Product> getAllProducts() {
-    return productService.getAllProducts();
+  public String getAllProducts(Model model) {
+    model.addAttribute("products", productService.getAllProducts());
+    return "products";
   }
 
   @GetMapping("/{id}")
-  public Product getProductById(@PathVariable int id) {
-    return productService.getById(id);
+  public String getProductById(@PathVariable int id, Model model) {
+    model.addAttribute("product", productService.getById(id));
+    return "product-info";
   }
 }
